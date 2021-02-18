1cb351a7b1f2663f82c1d8ebe55ad7560ab2e534
szymonbarabasz <79063933+szymonbarabasz@users.noreply.github.com>
2021-02-18 21:24:20 +0100

    master

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    window.scrollY //właściwość pokazująca ile scrollowaliśmy
-    document.documentElement.offsetHeight //wysokość całej strony
+document.addEventListener("DOMContentLoaded", () => {
+  window.scrollY; //właściwość pokazująca ile scrollowaliśmy - odjąć trzeba wysokość okna (window.innerHeight)
+  document.documentElement.offsetHeight; //wysokość całej strony
+  window.innerHeight; //wysokość okna przeglądarki
 
-    
-})
+  const status = document.querySelector(".status");
+
+  function statusbar() {
+    let currentScroll = Math.round(
+      (window.scrollY /
+        (document.documentElement.offsetHeight - window.innerHeight)) *
+        100
+    ); //
+    status.style.width = currentScroll + "%";
+  }
+
+  window.addEventListener("scroll", statusbar);
+});
