d7177ed43c220c9a5ff3ea7d051106c56be2cc24
szymonbarabasz <79063933+szymonbarabasz@users.noreply.github.com>
2021-02-18 21:11:38 +0100

    master

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    window.scrollY //właściwość pokazująca ile scrollowaliśmy
+    document.documentElement.offsetHeight //wysokość całej strony
+
+    
+})
